fddd044ea0a26feb95d67f0b7b5d9756538492f2
Akaash Venkat <akaashvenkat@gmail.com>
2020-08-05 01:57:50 -0700

    work on email signin/signup

--- ui/public/Javascript/signup_signin.js
+++ ui/public/Javascript/signup_signin.js
@@ -8,8 +8,8 @@ var toggleSignIn = function() {
     firebase.auth().signOut();
     // [END signout]
   } else {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
+    var email = document.getElementById('modal_email').value;
+    var password = document.getElementById('modal_password').value;
     if (email.length < 4) {
       alert('Please enter an email address.');
       return;
@@ -21,23 +21,24 @@ var toggleSignIn = function() {
     // Sign in with email and pass.
     // [START authwithemail]
     firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // [START_EXCLUDE]
-    if (errorCode === 'auth/wrong-password') {
-      alert('Wrong password.');
-    } else {
-      alert(errorMessage);
-    }
-    console.log(error);
-    //document.getElementById('quickstart-sign-in').disabled = false;
-    // [END_EXCLUDE]
-  });
-  // [END authwithemail]
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // [START_EXCLUDE]
+      if (errorCode === 'auth/wrong-password') {
+        alert('Wrong password.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+      //document.getElementById('quickstart-sign-in').disabled = false;
+      // [END_EXCLUDE]
+      return;
+    });
+    // [END authwithemail]
   }
   //document.getElementById('quickstart-sign-in').disabled = true;
-  console.log("success");
+  window.location.assign("./Webmark.html")
 }
 
 /**
@@ -67,9 +68,10 @@ var handleSignUp = function() {
       alert(errorMessage);
     }
     console.log(error);
+    return;
     // [END_EXCLUDE]
   });
-  // [END createwithemail]
+  window.location.assign("./Walkthrough.html")
 }
 
 /**
