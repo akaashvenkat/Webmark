f10822fa44377fd1f9aeee7c6d156aaf8828f44b
Akaash Venkat <akaashvenkat@gmail.com>
2020-09-23 00:57:23 -0700

    users can sign in from webmark page

--- src/static/javascript/email_auth.js
+++ src/static/javascript/email_auth.js
@@ -1,7 +1,9 @@
 (function() {
 
   var config = firebaseConfig;
-  firebase.initializeApp(config);
+  if (firebase.apps.length === 0) {
+    firebase.initializeApp(config);
+  }
 
   const emailSignUp = document.getElementById('signup_email');
   const passwordSignUp = document.getElementById('signup_password');
@@ -12,40 +14,51 @@
   const emailPasswordReset = document.getElementById('reset_password_email');
   const buttonPasswordReset = document.getElementById('reset_password_button');
 
-  buttonSignUp.addEventListener('click', e => {
-    const email = emailSignUp.value;
-    const pass = passwordSignUp.value;
-    const auth = firebase.auth();
+  if (buttonSignUp) {
+    buttonSignUp.addEventListener('click', e => {
+      const email = emailSignUp.value;
+      const pass = passwordSignUp.value;
+      const auth = firebase.auth();
 
-    if (email.length < 1 && pass.length < 1) {
-      alert('Please enter an email and a password.');
-      return;
-    } else if (email.length < 1) {
-      alert('Please enter an email.');
-      return;
-    } else if (pass.length < 1) {
-      alert('Please enter a password.');
-      return;
-    }
-
-    auth.createUserWithEmailAndPassword(email, pass)
-      .then(function(result) {
-        window.location.assign("./walkthrough")
-      })
-      .catch(function(error) {
-        if (error.code == 'auth/email-already-in-use') {
-          alert('The email is already in use. Please sign up with another email.');
-        } else if (error.code == 'auth/invalid-email') {
-          alert('The email address is not valid. Please use a valid email address.');
-        } else if (error.code == 'auth/weak-password') {
-          alert('The password is too weak. Please use a stronger password.');
-        } else {
-          alert(error.message);
-        }
-        console.log(error);
+      if (email.length < 1 && pass.length < 1) {
+        alert('Please enter an email and a password.');
         return;
+      } else if (email.length < 1) {
+        alert('Please enter an email.');
+        return;
+      } else if (pass.length < 1) {
+        alert('Please enter a password.');
+        return;
+      }
+
+      auth.createUserWithEmailAndPassword(email, pass)
+        .then(function(result) {
+          auth.currentUser.getIdToken(true)
+            .then(function (idToken) {
+              updateToken(idToken)
+                .then(function(result) {
+                  window.location.assign("./walkthrough")
+              });
+  	        })
+            .catch(function (error) {
+              self.text = "ERROR:" + error.message;
+          });
+        })
+        .catch(function(error) {
+          if (error.code == 'auth/email-already-in-use') {
+            alert('The email is already in use. Please sign up with another email.');
+          } else if (error.code == 'auth/invalid-email') {
+            alert('The email address is not valid. Please use a valid email address.');
+          } else if (error.code == 'auth/weak-password') {
+            alert('The password is too weak. Please use a stronger password.');
+          } else {
+            alert(error.message);
+          }
+          console.log(error);
+          return;
+      });
     });
-  });
+  }
 
   async function updateToken(tok) {
     sessionStorage.setItem('token', tok)
