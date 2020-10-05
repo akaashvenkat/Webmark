59df1c1507c130a87e652b0614d8ba4e3c8c7afc
Akaash Venkat <akaashvenkat@gmail.com>
2020-10-05 00:34:00 -0700

    adding config file for js

---
+++ src/static/config/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "${FIREBASE_API_KEY}",
+  authDomain: "${FIREBASE_AUTH_DOMAIN}",
+  databaseURL: "${FIREBASE_DATABASE_URL}",
+  projectId: "${FIREBASE_PROJECT_ID}",
+  storageBucket: "${FIREBASE_STORAGE_BUCKET}",
+  messagingSenderId: "${FIREBASE_MESSAGE_SENDER_ID}",
+  appId: "${FIREBASE_APP_ID}",
+  measurementId: "${FIREBASE_MEASUREMENT_ID}"
+};
