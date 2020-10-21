2387772a22eaa3258bf79e6db7450ab0786c6ce7
Akaash Venkat <akaashvenkat@gmail.com>
2020-10-20 23:41:57 -0700

    updating chromedriver args

--- src/ItemsAPI.py
+++ src/ItemsAPI.py
@@ -202,7 +202,8 @@ def get_base_64(url):
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-sh-usage')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--single-process')
     chrome_options.binary_location = environ["GOOGLE_CHROME_BIN"]
 
     driver = webdriver.Chrome(executable_path=environ["CHROMEDRIVER_PATH"], chrome_options=chrome_options)
